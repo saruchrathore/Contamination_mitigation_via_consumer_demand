9719fb2c6b6be40b2dbd12c9b85919611a068b28
saruchrathore <saruch.rathore@gmail.com>
2024-04-25 09:02:32 +0200

    run

---
+++ run_contamination_impact_mitigation.m
@@ -0,0 +1,27 @@
+clc; clear all; close all;
+
+%% Would you like to run simulation test or see the summary 
+
+prompt = ['Do you want to run the (1)simulation test or (2)see the summary statistic results?',...
+    '\nPress "1" for simulation test or "2" for summary statistic results.    '];
+selection = input(prompt);   % Ask for running open loop simulation
+disp(' ');
+
+if selection==1
+    disp('Running the simulation test. This might take some time')
+    cd m_files
+    main_simulation
+    cd ..
+elseif selection==2
+    disp('Displaying the summary statistic results')
+    cd m_files
+    % Run the summary statistic result code
+    summary_result
+    cd ..
+else
+    disp('Default: Displaying the summary statistic results')
+    cd m_files
+    % Run the summary statistic result code
+    summary_result
+    cd ..
+end
